05a339554cff373214a1d6ade2a5152fcbd1ca56
Howard Miller <howardsmiller@gmail.com>
2025-01-21 11:45:25 +0000

    changes on office laptop

---
+++ redis_conf/redis-cluster-create.sh
@@ -0,0 +1,11 @@
+# wait for the docker-compose depends_on to spin up the redis nodes usually takes this long
+sleep 10
+
+
+node_1_ip=$(getent hosts redis-cluster-node-0 | awk '{ print $1 }')
+
+
+node_2_ip=$(getent hosts redis-cluster-node-1 | awk '{ print $1 }')
+
+
+redis-cli --cluster create $node_1_ip:6379 $node_2_ip:6379 ... --cluster-replicas 1 --cluster-yes
